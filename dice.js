d9900c048c64b2fe8632e635050d06d2f8889cf0
Alle Nithish Reddy <124487408+alle-nithish@users.noreply.github.com>
2024-07-04 23:22:06 +0530

    Update dice.js

--- dice.js
+++ dice.js
@@ -1,42 +1,11 @@
 var randomNum1 = Math.floor(Math.random() * 6) + 1;
 var randomNum2 = Math.floor(Math.random() * 6) + 1;
-if(randomNum1 === 1){
-    document.querySelector(".img1").setAttribute("src", "./images/dice1.png");
-}
-else if(randomNum1 === 2){
-    document.querySelector(".img1").setAttribute("src", "./images/dice2.png");
-}
-else if(randomNum1 === 3){
-    document.querySelector(".img1").setAttribute("src", "./images/dice3.png");
-}
-else if(randomNum1 === 4){
-    document.querySelector(".img1").setAttribute("src", "./images/dice4.png");
-}
-else if(randomNum1 === 5){
-    document.querySelector(".img1").setAttribute("src", "./images/dice5.png");
-}
-else {
-    document.querySelector(".img1").setAttribute("src", "./images/dice6.png");
-}
 
-if(randomNum2 === 1){
-    document.querySelector(".img2").setAttribute("src", "./images/dice1.png");
-}
-else if(randomNum2 === 2){
-    document.querySelector(".img2").setAttribute("src", "./images/dice2.png");
-}
-else if(randomNum2 === 3){
-    document.querySelector(".img2").setAttribute("src", "./images/dice3.png");
-}
-else if(randomNum2 === 4){
-    document.querySelector(".img2").setAttribute("src", "./images/dice4.png");
-}
-else if(randomNum2 === 5){
-    document.querySelector(".img2").setAttribute("src", "./images/dice5.png");
-}
-else {
-    document.querySelector(".img2").setAttribute("src", "./images/dice6.png");
-};
+var diceImage1 = "dice" + randomNum1 + ".png";
+var diceImage2 = "dice" + randomNum2 + ".png";
+
+document.querySelector(".img1").setAttribute("src", "./images/" + diceImage1);
+document.querySelector(".img2").setAttribute("src", "./images/" + diceImage2);
 
 if(randomNum1 > randomNum2){
     document.querySelector("h1").innerHTML="Player1 Wins!";
